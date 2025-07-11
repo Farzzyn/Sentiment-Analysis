a0a34b001419e8785bf45264b197b9df97494a9d
Muhammed farzin <mhdfarzin121@gmail.com>
2025-07-11 12:52:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+
+import streamlit as st
+import joblib
+
+# Load model and vectorizer
+model = joblib.load('svm_model.pkl')
+vectorizer = joblib.load('tfidf_vectorizer.pkl')
+
+
+st.title("Customer Review Sentiment Analyzer")
+user_input = st.text_area("Enter a customer review:")
+
+
+if st.button("Analyze Sentiment"):
+    if user_input.strip() != "":
+        vectorized_input = vectorizer.transform([user_input])
+        prediction = model.predict(vectorized_input)[0]
+
+        if prediction == 1:
+            st.success("✅ Positive Review")
+        elif prediction == 0:
+            st.info("😐 Neutral Review")
+        else:
+            st.error("❌ Negative Review")
+    else:
+        st.warning("Please enter some text.")
